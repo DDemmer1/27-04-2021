a5a72cbbe2832e2d6ecec37bff0035c9afeb0783
DDemmer1 <Dennis.Demmer@web.de>
2021-04-27 09:06:19 +0200

    initial commit

---
+++ src/de/demmer/dennis/Application.java
@@ -0,0 +1,24 @@
+package de.demmer.dennis;
+
+import java.util.Random;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		
+		//Erstellt hier eure Liste
+		
+		Random rnd = new Random();
+		for (int i = 0; i < 1000; i++) {
+//			nameEurerListe.add(rnd.nextInt(101));
+		}
+		
+		
+		
+		//Hier könnt ihr mit der Liste arbeiten
+		
+		
+	}
+
+}
